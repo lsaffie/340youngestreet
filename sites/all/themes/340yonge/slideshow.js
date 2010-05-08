3d35a1148c4dab1506190112ec8394c2367cc896
Luis Saffie <luis.saffie@ehealthinnovation.org>
2010-05-07 22:03:01 -0400

    Deleted unused modules and CSS changes

--- sites/all/themes/340yonge/slideshow.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-  $('#st_slogan .view-content').cycle({ 
-  //$('.view-content').cycle({ 
-    fx: 'scrollLeft'; // You can change the effect at this line
-  });
-});
