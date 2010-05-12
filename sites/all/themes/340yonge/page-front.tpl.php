dcd54678a12e529770b4dc25ec2002813311f7dd
Luis Saffie <luis.saffie@ehealthinnovation.org>
2010-05-11 21:27:13 -0400

    Adding logo and front page

--- sites/all/themes/340yonge/page-front.tpl.php
+++ sites/all/themes/340yonge/page-front.tpl.php
@@ -18,7 +18,6 @@
   <table border="0" cellpadding="0" cellspacing="0" id="header">
     <tr>
       <td id="logo">
-        <?php if ($logo) { ?><a href="<?php print $front_page ?>" title="<?php print t('Home') ?>"><img src="<?php print $logo ?>" alt="<?php print t('Home') ?>" /></a><?php } ?>
         <?php if ($site_slogan) { ?><div class='site-slogan'><?php print $site_slogan ?></div><?php } ?>
       </td>
     
@@ -31,6 +30,9 @@
   </table>
 
   <?php if ($site_name) { ?><h1 class='site-name'><a href="<?php print $front_page ?>" title="<?php print t('Home') ?>"><?php print $site_name ?></a></h1><?php } ?>
+  <div id="logo">
+    <?php if ($logo) { ?><a href="<?php print $front_page ?>" title="<?php print t('Home') ?>"><img src="<?php print $logo ?>" alt="<?php print t('Home') ?>" /></a><?php } ?>
+  </div>
   <?php print $header ?>
 
   <table border="0" cellpadding="0" cellspacing="0" id="content">
